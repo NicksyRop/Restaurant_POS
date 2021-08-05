38bd5fd01974b37f3ab734426e5080ffda7dedd4
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-05 12:21:05 +0300

    added user managent,cashier and report icons to the home view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-header"> <i class="fas fa-user mr-4"></i>{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+            <div class="row mt-5">
+                <div class="col-12 col-md-4 mb-4 border border-1">
+                    <h4>Data Managent</h4>
+                    <img src="{{ asset('images/folder-management.svg')}}" class="img-fluid" alt="Data management image">
+                </div>
+                <div class="col-12 col-md-4 mb-4 border border-1">
+                    <h4>Cashier</h4>
+                    <img src="{{ asset('images/cashier.svg')}}" class="img-fluid" alt="Cashier Image">
+                </div>
+                <div class="col-12 col-md-4 mb-4 rounded border border-1">
+                    <h4>Report</h4>
+                    <img src="{{ asset('images/report.svg')}}" class="img-fluid" alt="Report image">
                 </div>
             </div>
         </div>
