e216ca3f6a37ea5b29b6081313325825de72b1a4
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-06 12:59:16 +0300

    added a button with a click enent to fetch all the tables using jquery

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -2,8 +2,24 @@
 
 @section('content')
 
-<div class="conatiner">
-    <h4>Hello Cashier</h4>
+<div class="container">
+<div class="row">
+    <div class="col d-flex justify-content-center">
+        <button class="btn btn-primary " id="btn-show-tables">View All Tables</button>
+    </div>
+</div>
 </div>
 
 @endsection
+@section('scripts')
+<script>
+    $(document).ready(function(){
+
+    $('#btn-show-tables').click(function(){
+
+
+    })
+    })
+</script>
+@endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -85,5 +85,6 @@
         </main>
     </div>
     @notifyJs
+@yield('scripts')
 </body>
 </html>
