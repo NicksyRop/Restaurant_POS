2dca53a2c9dc098a9531476afe2ec911bd8ced4e
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-06 11:43:57 +0300

    added a background image to the index of managment page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    @yield('css')
     @notifyCss
 </head>
 <body>
--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -2,23 +2,35 @@
 
 @section('content')
 
-<div class="container">
-  <div class="row">
-    <div class="col-12 col-md-4">
-        <ul class="list-goup-item">
-            <li class="list-group-item"><a href="/managent/category"><i class="fas fa-bars mr-3"></i>Category</a></li>
-            <li class="list-group-item"><a href=""> <i class="fas fa-hamburger mr-3"></i> Menu</a></li>
-            <li class="list-group-item"><a href=""><i class="fas fa-user mr-3"></i>User</a></li>
-            <li class="list-group-item"><a href=""><i class="fas fa-table mr-3"></i>Table</a></li>
+    <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4">
+              <ul class="list-goup-item">
+                  <li class="list-group-item"><a href="/managent/category"><i class="fas fa-bars mr-3"></i>Category</a></li>
+                  <li class="list-group-item"><a href=""> <i class="fas fa-hamburger mr-3"></i> Menu</a></li>
+                  <li class="list-group-item"><a href=""><i class="fas fa-user mr-3"></i>User</a></li>
+                  <li class="list-group-item"><a href=""><i class="fas fa-table mr-3"></i>Table</a></li>
 
-        </ul>
-    </div>
-    <div class="col-12 col-md-8">
+              </ul>
+          </div>
+
+        </div>
+
+      </div>
+
+@endsection
+
+@section('css')
+<style>
+    body{
+        background-image: url('{{ asset('images/hotel.jpg')}}');
+
+        background-size: cover;
+        background-repeat :no-repeat;
 
 
-    </div>
-  </div>
+    }
+    </style>
 
-</div>
 
 @endsection
