5a69469cc48f1a5fe5135df860ebfccdd5a72c80
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-20 18:12:29 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') == 'production'){
+
+          \URL::forceScheme('https');
+        }
     }
 }
