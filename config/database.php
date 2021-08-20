6814d88cd0f9dd9a409b9fc1c9d71dec4f5c3519
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-20 17:57:24 +0300

    changed database charset to utf8

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
